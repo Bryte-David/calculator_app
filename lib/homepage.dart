964d69d87d4bd5639ff9db4bb82c18eb5bb0598f
Bryte David <brytedree@gmail.com>
2020-12-19 03:49:20 +0100

    init

---
+++ lib/homepage.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var num1=0,num2=0,sum;
+
+  final TextEditingController t1 = new TextEditingController(text: "0");
+  final TextEditingController t2 = new TextEditingController(text: "0");
+  
+  void doAddition(){
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 + num2;
+    });
+  }
+
+  void doSubtraction(){
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 - num2;
+    });
+  }
+
+  void doMul(){
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 * num2;
+    });
+  }
+
+  void doDiv(){
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 ~/ num2;
+    });
+  }
+
+  doClear(){
+    setState(() {
+      t1.text = "0";
+      t2.text = "0";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Calculator"),
+      ),
+      body: new Container(
+        padding: const EdgeInsets.all(40.0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Output : $sum",
+            style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.purple),
+            ),
+            new TextField(
+              controller: t1,
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(
+                hintText: "Enter Number 1"
+              ),
+            ),
+
+            new TextField(
+              controller: t2,
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(
+                hintText: "Enter Number 2"
+              ),
+            ),
+            new Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text("+"),
+                  color: Colors.greenAccent,
+                  onPressed: doAddition,
+                ),
+
+                new MaterialButton(
+                  child: new Text("-"),
+                  color: Colors.greenAccent,
+                  onPressed: doSubtraction,
+                ),
+
+              ],
+            ),
+            new Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+            ),
+            
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text("*"),
+                  color: Colors.greenAccent,
+                  onPressed: doMul,
+                ),
+
+                new MaterialButton(
+                  child: new Text("/"),
+                  color: Colors.greenAccent,
+                  onPressed: doDiv,
+                ),
+
+              ],
+            ),
+             new Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+            ),
+            
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text("/"),
+                  color: Colors.greenAccent,
+                  onPressed: doDiv,
+                ),
+              ],
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
